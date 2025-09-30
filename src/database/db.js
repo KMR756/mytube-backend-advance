1458ee43b626ab89f5986dbf61f56029434d0a66
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-09-30 22:45:05 +0600

    commection sever with express

--- src/database/db.js
+++ src/database/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
-      `\n MongoDB connected!! DB host: ${connectionInstance.connection.host}`
+      `MongoDB connected!! DB host: ${connectionInstance.connection.host}`
     );
   } catch (error) {
     console.log("mongoDB connection faild!", error);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./database/db.js";
+import { app } from "./app.js";
 
-connectDB();
+const PORT = process.env.PORT || 3000;
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running at port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection faild!!", err);
+  });
